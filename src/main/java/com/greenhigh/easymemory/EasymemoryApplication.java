963e5c982b230ae7d88a0c64b054529bd3c12482
GreanNeuron <goodev@kakao.com>
2022-07-22 05:45:36 +0900

    first commit

---
+++ src/main/java/com/greenhigh/easymemory/EasymemoryApplication.java
@@ -0,0 +1,13 @@
+package com.greenhigh.easymemory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasymemoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasymemoryApplication.class, args);
+	}
+
+}
