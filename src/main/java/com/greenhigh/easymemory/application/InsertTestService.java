694b9dc826dd734ed8a5196cf3a602e8c93357f1
GreanNeuron <goodev@kakao.com>
2022-07-31 01:45:43 +0900

    (test):#3-refactor-port가 interface가 되도록 수정

--- src/main/java/com/greenhigh/easymemory/application/InsertTestService.java
+++ src/main/java/com/greenhigh/easymemory/application/InsertTestService.java
@@ -1,29 +1,22 @@
 package com.greenhigh.easymemory.application;
 
-import com.greenhigh.easymemory.adapter.out.persistence.InsertTestRepository;
-import com.greenhigh.easymemory.application.port.in.InsertTestUseCase;
 import com.greenhigh.easymemory.application.port.out.InsertTestPort;
-import com.greenhigh.easymemory.domain.Member;
+import com.greenhigh.easymemory.application.port.in.InsertTestUseCase;
+import com.greenhigh.easymemory.adapter.out.persistence.InsertTestRepository;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
 
 @Slf4j
 @Service
 public class InsertTestService implements InsertTestUseCase {
-    private final InsertTestRepository insertTestRepository;
+    private final InsertTestPort insertTestPort;
 
     public InsertTestService() {
-       insertTestRepository = new InsertTestPort();
+       insertTestPort = new InsertTestRepository();
     }
 
     @Override
     public String insertTest() {
-        return insertTestRepository.insertTest();
+        return insertTestPort.insertTest();
     }
 }
